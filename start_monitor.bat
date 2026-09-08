18cdd06534b36d061e5c2ada29daa9ecaef880ab
WorkBuddy <rebate@local>
2026-09-09 06:56:43 +0800

    fix: start_monitor.bat改为直接启动pythonw(计划任务已删除,Boss要求纯手动)

--- start_monitor.bat
+++ start_monitor.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Starting linebao monitor (hidden background)...
-schtasks /run /tn LinebaoMonitor
+cd /d E:\Jingdong\rebate-site
+start "" "C:\Users\23058\.workbuddy\binaries\python\envs\default\Scripts\pythonw.exe" -u incremental.py --interval 30
 echo.
 echo Started. Log: E:\Jingdong\rebate-site\monitor_log.txt
 echo (view log: check_log.bat  /  stop: stop_monitor.bat)
